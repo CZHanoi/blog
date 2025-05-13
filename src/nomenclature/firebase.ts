54869d1665a8fe07c6dc16c9cc269d5ce452e55f
CZHanoi <21301010003@m.fudan.edu.cn>
2025-05-13 17:07:37 +0800

    czh

--- src/nomenclature/firebase.ts
+++ src/nomenclature/firebase.ts
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+/* 读取 Vite 环境变量（注：必须以 VITE_ 前缀） */
 const firebaseConfig = {
-  apiKey: "AIzaSyCNKn3Vv6a8t1ED_suSFlkjs9t5CKFEczs",
-  authDomain: "blogczh.firebaseapp.com",
-  projectId: "blogczh",
-  storageBucket: "blogczh.firebasestorage.app",
-  messagingSenderId: "138673168059",
-  appId: "1:138673168059:web:2b8d484a7f43842524139e",
-  measurementId: "G-64EW6CCBF6"
+  apiKey:               import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:           import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:            import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:        import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:    import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:                import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export let remoteEnabled = true;
