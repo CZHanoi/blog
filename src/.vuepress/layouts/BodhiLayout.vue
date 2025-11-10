36045dbba4c0d1637640d0c128a98bd35393de85
CZHanoi <21301010003@m.fudan.edu.cn>
2025-11-10 16:18:52 +0800

    1

--- src/.vuepress/layouts/BodhiLayout.vue
+++ src/.vuepress/layouts/BodhiLayout.vue
@@ -303,8 +303,10 @@ onUnmounted(()=>{
   <div class="bodhi-layout">
     <div class="bodhi-page">
       <header class="hdr">
-        <div class="title">Bodhi · 本学期课程</div>
-        <div class="meta">起始周一：{{ SEMESTER_START_ISO }}</div>
+        <!-- <div class="title">Bodhi · 本学期课程</div>
+        <div class="meta">起始周一：{{ SEMESTER_START_ISO }}</div> -->
+        <div class="title">ASHOKA ERA</div>
+        <div class="meta">是诸法空相，不生不灭，不垢不净，不增不减；</div>
       </header>
 
       <section class="board" ref="boardRef">
